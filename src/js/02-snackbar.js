cb4986c354cf89eaef237b76533a4c98184325c4
FunnyBilbur <tonistark1997@gmail.com>
2023-12-22 17:48:47 +0300

    snackbar is done

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,43 +1,54 @@
+import iziToast from "izitoast";
+import "iziToast/dist/css/iziToast.min.css";
+import "iziToast/dist/js/iziToast.min.js";
+
 const form = document.querySelector('.form');
-const input = document.querySelector('.input__delay');
-// const input = document.querySelector('.input__delay');
-// console.dir(input.textContent);
+const input_delay = document.querySelector('.input__delay');
+const input_fulfilled = document.querySelector('.input__fulfilled');
+const input_rejected = document.querySelector('.input__rejected');
+
+
 
 form.addEventListener('submit', (arg) => {
     arg.preventDefault();
+    const delay_ms = input_delay.value;
     console.log('Submitted');
-    // const delay_value = arg.currentTarget.children[0].children[0].value;
-    console.log(input.value);
+    if (input_fulfilled.checked) {
+        makePromise({ shouldResolve: true, delay: delay_ms })
+            .then(result => console.log(result))
+            .catch(error => console.error(error));
 
+    } else {
+        makePromise({ shouldResolve: false, delay: delay_ms })
+            .then(result => console.log(result))
+            .catch(error => console.error(error));
+    }
 });
-// const makeGreeting = guestName => {
-//     if (!guestName) {
-//         return Promise.reject("Guest name must not be empty");
-//     } else {
-//         return Promise.resolve(`Welcome ${guestName}`);
-//     }
-// };
-// makeGreeting("Dmytrii").then(greeting => console.log(greeting)).catch(error => console.error(error));
-const makePromise = ({ value, delay, shouldResolve = true }) => {
+
+const makePromise = ({ shouldResolve, delay }) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (shouldResolve) {
-                resolve(value);
+                resolve(`✅ Fulfilled promise in ${delay}ms`);
+                iziToast.show({
+                    titleColor: 'white',
+                    position: 'topRight',
+                    message: `✅ Fulfilled promise in ${delay}ms`,
+                    messageColor: 'white',
+                    backgroundColor: '#59A10D',
+                    progressBarColor: '#B5EA7C'
+                });
             } else {
-                reject(value);
+                reject(`❌ Rejected promise in ${delay}ms`);
+                iziToast.show({
+                    titleColor: 'white',
+                    position: 'topRight',
+                    message: `❌ Rejected promise in ${delay}ms`,
+                    messageColor: 'white',
+                    backgroundColor: '#EF4040',
+                    progressBarColor: '#B51B1B'
+                });
             }
         }, delay);
     });
 };
-makePromise({ value: "Some value", delay: 3000 })
-    .then(result => console.log(result))
-    .catch(error => console.error(error));
-makePromise({ value: "A", delay: 1000 })
-    .then(value => console.log(value)) // "A"
-    .catch(error => console.error(error));
-makePromise({ value: "B", delay: 4000 })
-    .then(value => console.log(value)) // "B"
-    .catch(error => console.error(error));
-makePromise({ value: "C", delay: 2000, shouldResolve: false })
-    .then(value => console.log(value))
-    .catch(error => console.error(error)); // "C"
